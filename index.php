74726ff1a9ee92d04962053d8df0a7d350e8b119
uenoryo <r.r.ag0611@outlook.jp>
2016-09-04 13:37:34 +0900

    基本スタイル実装

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
 </head>
 <body>
 <?php require_once('header.php');?>
-<?php require_once('mainbody.php');?>
+<div class="container">
+    <?php require_once('mainbody.php');?>
+    <?php require_once('sidebar.php');?>
+</div>
 <?php require_once('footer.php');?>
 </body>
 </html>
